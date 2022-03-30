d0a3e2cadf8a400a3e1768f15427f2c5a7a935d4
andreagiodi <giodice.andrea@iisgalvanimi.edu.it>
2022-03-30 12:55:08 +0000

    commit

--- verificaA.py
+++ verificaA.py
@@ -17,18 +17,46 @@ stazioni = pd.read_csv("/workspace/archivioverifiche/coordfix_ripetitori_radiofo
 
 quartieri = gpd.read_file("/workspace/archivioverifiche/ds964_nil_wm.zip")
 
+stazioni = gpd.GeoDataFrame(stazioni, geometry=gpd.points_from_xy(stazioni['LONG._decimal'], stazioni['LAT._decimal']))
+
+stazioni.crs = "EPSG:4326"
+stazioni.to_crs(epsg=32632)
+stazioni = stazioni.drop(columns=['LAT._decimal', 'LONG._decimal','Location'])
+
 
 @app.route('/', methods=['GET'])
 def home():
     
     return render_template('home.html')
 
+
 @app.route('/numero', methods=['GET'])
 def numero():
     #numero stazioni per ogni municipio
+    
     risultato = stazioni.groupby('MUNICIPIO')['OPERATORE'].count().reset_index()
+    
 
     return render_template('elenco.html' ,risultato= risultato.to_html())
+
+
+@app.route('/input', methods=['GET'])
+def homeinput():
+    
+    return render_template('homeinput.html')
+
+
+@app.route('/rispinput', methods=['GET'])
+def input():
+    value = request.args['value']
+    risp = quartieri[quartieri.NIL.str.contains(value)]
+    stazquar = stazioni[stazioni.within(risp.geometry.squeeze())]
+    return render_template('input.html',a=stazquar.to_html())
+
+@app.route('/dropdown', methods=['GET'])
+def homedropdown():
+    
+    return render_template('input.html',a=stazquar.to_html())
 
 
 
